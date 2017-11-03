e763df9079e5ead321349b46bf3c5e78909e0c42
Oriebir <32739406+Oriebir@users.noreply.github.com>
2017-11-03 02:41:32 +0000

    Add files via upload
    Corrigi a opcção "anterior"
    Agora lista nomes e datas a partir da base de dados
    Não reconhece caracteres especiais

--- listUsers.php
+++ listUsers.php
@@ -2,6 +2,9 @@
 $pageNumber = $_GET["pageNumber"]; /*número de paginação*/
 $pageSize = $_GET["pageSize"]; /*número de utilizadores/linhas*/
 
+$link = mysqli_connect('localhost', 'root', '', 'SIM') or die(mysqli_error($link));
+
+//$number = mysqli_num_rows($get_user);
 ?>
 
 <table width="500px" height="350px" border="1" align="center" >
@@ -13,18 +16,26 @@ $pageSize = $_GET["pageSize"]; /*número de utilizadores/linhas*/
 			<tr>
 				<?php 
 					for($n=($pageNumber - 1) * $pageSize; $n < $pageNumber * $pageSize; $n++) { /*n é o index*/
+					
+					$i=$n+1;
+					$query_id = 'SELECT * FROM Users WHERE (ID = "'.$i.'")';
+					$get_id = mysqli_query($link, $query_id) or die(mysqli_error($link));
+					$id_info = mysqli_fetch_array($get_id);
+					$name=$id_info['Name'];
+					$date=$id_info['Creation_Date'];
+						
 						echo "<tr>";
 
 							echo "<td>";
-								echo "$n";
+								echo "$i";
 							echo "</td>";
 
 							echo "<td>";
-								echo "";
+								echo "$name";
 							echo "</td>";
 
 							echo "<td>";
-								echo "";
+								echo "$date";
 							echo "</td>";
 						echo "</tr>";
 					}
@@ -37,7 +48,7 @@ $pageSize = $_GET["pageSize"]; /*número de utilizadores/linhas*/
 
 <?php
 	$png = $pageNumber + 1;
-	$pnd = $png -1;
+	$pnd = $pageNumber - 1;
 
 	echo "<a href='http://localhost/SIM/index.php?operacao=listUsers&pageNumber=$pnd&pageSize=10'>Anterior </a>"; //voltar para trás em progresso
 
