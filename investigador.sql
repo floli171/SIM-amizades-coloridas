e11af23faf5a43398b4da571d927caf56a2df1de
Oriebir <32739406+Oriebir@users.noreply.github.com>
2017-11-21 02:15:07 +0000

    Add files via upload

---
+++ investigador.sql
@@ -0,0 +1,45 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: 21-Nov-2017 às 02:04
+-- Versão do servidor: 5.7.19
+-- PHP Version: 5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sim`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `investigador`
+--
+
+DROP TABLE IF EXISTS `investigador`;
+CREATE TABLE IF NOT EXISTS `investigador` (
+  `I_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Nome` varchar(100) NOT NULL,
+  `Username` varchar(32) NOT NULL,
+  `Password` varchar(20) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `Data de Registo` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`I_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Investigadores';
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
