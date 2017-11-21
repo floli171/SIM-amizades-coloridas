8a28794a4d385c3f01bef41cd9406cde27bde88c
floli171 <32899663+floli171@users.noreply.github.com>
2017-11-21 01:22:24 +0000

    Delete personalInfo.php

--- personalInfo.php
+++
@@ -1,4 +0,0 @@
-Aqui deverá fazer o registo diário da sua alimenta&ccedil;&atilde;o
-
-<!-- sugestões
-		a partir do momento em que o utilizador coloca a informação há um lock e para alterar é necessário clicar no botão "editar"-->
